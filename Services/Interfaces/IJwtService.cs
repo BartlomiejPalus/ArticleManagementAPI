fe8294358a0bd1c88f7df1d3fd9eef6cdb86c2c0
Bartłomiej Palus <bartlomiej.palus@poczta.onet.pl>
2025-08-28 01:34:25 +0200

    Add JWT service

---
+++ Services/Interfaces/IJwtService.cs
@@ -0,0 +1,9 @@
+﻿using ArticleManagementAPI.Models;
+
+namespace ArticleManagementAPI.Services.Interfaces
+{
+	public interface IJwtService
+	{
+		string GenerateToken(User user);
+	}
+}
---
+++ Services/JwtService.cs
@@ -0,0 +1,46 @@
+﻿using ArticleManagementAPI.Models;
+using ArticleManagementAPI.Services.Interfaces;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace ArticleManagementAPI.Services
+{
+	public class JwtService : IJwtService
+	{
+		private readonly IConfiguration _configuration;
+
+		public JwtService(IConfiguration configuration)
+		{
+			_configuration = configuration;
+		}
+
+		public string GenerateToken(User user)
+		{
+			string secretKey = _configuration["Jwt:Secret"]!;
+			var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
+			var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256Signature);
+
+			var tokenDescriptor = new SecurityTokenDescriptor
+			{
+				Subject = new ClaimsIdentity(
+				[
+					new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+					new Claim(JwtRegisteredClaimNames.Email, user.Email),
+					new Claim(ClaimTypes.Role, user.Role.ToString())
+				]),
+				Expires = DateTime.UtcNow.AddMinutes(_configuration.GetValue<int>("Jwt:ExpirationInMinutes")),
+				SigningCredentials = credentials,
+				Issuer = _configuration["Jwt:Issuer"],
+				Audience = _configuration["Jwt:Audience"]
+			};
+
+			var handler = new JwtSecurityTokenHandler();
+
+			var token = handler.CreateToken(tokenDescriptor);
+
+			return handler.WriteToken(token);
+		}
+	}
+}
