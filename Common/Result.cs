6fcaacb5b4558c31ffb5e349bcc8532702e6aac8
Bartłomiej Palus <bartlomiej.palus@poczta.onet.pl>
2025-09-03 23:53:02 +0200

    Add ResultExtensions to map Result to IActionResult

--- Common/Result.cs
+++ Common/Result.cs
@@ -1,4 +1,5 @@
 ﻿using ArticleManagementAPI.Enums;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ArticleManagementAPI.Common
 {
@@ -33,5 +34,23 @@ namespace ArticleManagementAPI.Common
 		public static Result<T> Success(T value) => new (true, null, null, value);
 
 		public static new Result<T> Failure(ErrorType error, string message) => new (false, error, message, default);
+	}
+
+	public static class ResultExtensions
+	{
+		public static IActionResult ToErrorActionResult(this Result result, ControllerBase controller)
+		{
+			if (result.IsSuccess)
+				throw new InvalidOperationException("Cannot map a successful result to an error response.");
+
+			return result.ErrorType switch
+			{
+				ErrorType.BadRequest => controller.BadRequest(result.ErrorMessage),
+				ErrorType.Unauthorized => controller.Unauthorized(result.ErrorMessage),
+				ErrorType.NotFound => controller.NotFound(result.ErrorMessage),
+				ErrorType.InternalServerError => controller.StatusCode(500, result.ErrorMessage),
+				_ => controller.StatusCode(500, "Unexpected error")
+			};
+		}
 	}
 }
