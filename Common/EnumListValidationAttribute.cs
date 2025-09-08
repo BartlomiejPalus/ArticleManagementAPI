a00b0a9426eecaf985b197b5eb02d501b7152ed4
Bartłomiej Palus <bartlomiej.palus@poczta.onet.pl>
2025-09-08 22:42:52 +0200

    Add AddArticleDto and EnumListValidationAttribute

---
+++ Common/EnumListValidationAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.ComponentModel.DataAnnotations;
+
+namespace ArticleManagementAPI.Common
+{
+	public class EnumListValidationAttribute : ValidationAttribute
+	{
+		private readonly Type _type;
+
+		public EnumListValidationAttribute(Type type)
+		{
+			_type = type;
+		}
+
+		protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+		{
+			if (value is IEnumerable enumerable)
+			{
+				foreach (var item in enumerable)
+				{
+					if (!Enum.IsDefined(_type, item))
+						return new ValidationResult($"Invalid value {item} for enum {_type}");
+				}
+			}
+
+			return ValidationResult.Success;
+		}
+	}
+}
---
+++ DTOs/Article/AddArticleDto.cs
@@ -0,0 +1,19 @@
+﻿using ArticleManagementAPI.Common;
+using ArticleManagementAPI.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace ArticleManagementAPI.DTOs.Article
+{
+	public class AddArticleDto
+	{
+		[Required]
+		public string Title { get; set; }
+		[Required]
+		public string Content { get; set; }
+
+		[Required]
+		[MinLength(1)]
+		[EnumListValidationAttribute(typeof(ArticleCategory))]
+		public List<int> CategoryIds { get; set; } = [];
+	}
+}
