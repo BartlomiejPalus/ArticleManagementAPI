63397856b506698b4685ea5a990306b3c667377e
Bartłomiej Palus <bartlomiej.palus@poczta.onet.pl>
2025-10-09 01:17:46 +0200

    Add Result class

---
+++ ArticleManagement.Desktop/Common/Result.cs
@@ -0,0 +1,32 @@
+﻿namespace ArticleManagement.Desktop.Common
+{
+	public class Result
+	{
+		public bool IsSuccess { get; }
+		public string? ErrorMessage { get; }
+
+		protected Result(bool isSuccess, string? message)
+		{
+			IsSuccess = isSuccess;
+			ErrorMessage = message;
+		}
+
+		public static Result Success() => new(true, null);
+
+		public static Result Failure(string message) => new(false, message);
+	}
+
+	public class Result<T> : Result
+	{
+		public T? Value { get; }
+
+		protected Result(bool isSuccess, string? message, T? value) : base(isSuccess, message)
+		{
+			Value = value;
+		}
+
+		public static Result<T> Success(T value) => new(true, null, value);
+
+		public static new Result<T> Failure(string message) => new(false, message, default);
+	}
+}
